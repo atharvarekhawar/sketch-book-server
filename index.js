8cabc748940da5f0dedd15206828b58bd9affdc4
Atharva Rekhawar <atharvarekhawar64@gmail.com>
2023-10-01 19:14:27 +0530

    added socket events for client

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const { log } = require("console");
 
 const app = express();
 app.use(cors({origin: 'http://localhost:3000'}))
-    
+
 const httpServer = createServer(app);
 
 const io = new Server(httpServer,{cors:'http://localhost:3000'});
 
 io.on("connection", (socket) => {
     console.log("server was connected ");
+    socket.on('beginPath',(arg)=>{
+        socket.broadcast.emit('beginPath',arg);
+    });
+
+    socket.on('drawLine',(arg)=>{
+        socket.broadcast.emit('drawLine',arg);
+    });
+
+    socket.on('changeConfig',(arg)=>{
+        socket.broadcast.emit('changeConfig',arg);
+    })
 });
 
 httpServer.listen(5000);
